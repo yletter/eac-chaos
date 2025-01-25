b51f6da6b950b8066e12be44c7c32e71b22c29eb
yuvarajengineer <yuvarajengineer@gmail.com>
2025-01-25 18:52:08 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "eks-yuvaraj"
     region = "us-east-1"
-    key = "eac-eks/terraform.tfstate"
+    key = "eac-chaos/terraform.tfstate"
     profile = "saml"
   }
 }
