1944425a68fe67a96bb1de4a48cd256761c4824a
yuvarajengineer <yuvarajengineer@gmail.com>
2025-02-03 09:53:56 +0530

    remove sn

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -40,7 +40,6 @@ resource "aws_db_instance" "chaos_db" {
   skip_final_snapshot    = true
   storage_type           = "gp2"
   vpc_security_group_ids = [aws_security_group.chaos_sg.id]  # Ensure this is in the same VPC
-  subnet_group_name      = aws_db_subnet_group.chaos_subnet_group.name  # Use a subnet group in the correct VPC
 }
 
 output "aurora_endpoint" {
