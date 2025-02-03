d31a6bb20e60f735f5f1935183e03e402717ed21
yuvarajengineer <yuvarajengineer@gmail.com>
2025-02-03 09:38:29 +0530

    subnet_group_name

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,5 +1,5 @@
 # Create a Security Group for Aurora
-resource "aws_security_group" "aurora_sg" {
+resource "aws_security_group" "chaos_sg" {
   name        = "aurora-mysql-sg"
   description = "Allow MySQL inbound traffic"
 
@@ -21,7 +21,7 @@ resource "aws_security_group" "aurora_sg" {
 }
 
 # Create a Subnet Group
-resource "aws_db_subnet_group" "aurora_subnet_group" {
+resource "aws_db_subnet_group" "chaos_subnet_group" {
   name       = "aurora-subnet-group"
   subnet_ids = module.vpc.private_subnets  # Replace with your subnet IDs
 }
@@ -37,8 +37,10 @@ resource "aws_db_instance" "chaos_db" {
   publicly_accessible    = true
   username               = "adminuser"
   password               = "Sunday!20250202"
-  vpc_security_group_ids = [aws_security_group.aurora_sg.id]
   skip_final_snapshot    = true
+  storage_type           = "gp2"
+  vpc_security_group_ids = [aws_security_group.chaos_sg.id]  # Ensure this is in the same VPC
+  subnet_group_name      = aws_db_subnet_group.chaos_subnet_group.name  # Use a subnet group in the correct VPC
 }
 
 output "aurora_endpoint" {
